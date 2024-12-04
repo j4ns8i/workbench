52569f93f2079043ff78646c800c5ca0d0e170fe
j4ns8i <jsmalkowski+github@gmail.com>
2024-12-03 20:17:11 -0500

    chore: use go-task instead of devenv for task running
    The idea behind devenv is nice, but as long as reloading any changes
    takes seconds to minutes, it's terrible to loop into the development
    cycle.
    
    It felt like fitting a square peg in a round hole a lot of the time,
    e.g. status checks that write to stdout count as failed. I think it's
    trying to do too much, between scripts, tasks, and processes. I'll just
    use it for package management and direnv hooks for now.

--- devenv.nix
+++ devenv.nix
@@ -1,40 +1,7 @@
 { pkgs, lib, config, inputs, ... }:
   let
     pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
-    targets = {
-      "build:api-image" = {
-        exec = "docker build ./api -f api/build/Dockerfile -t workbench/api";
-      };
-      "build:helm" = {
-        exec = "helm template local deploy/ --release-name --output-dir=deploy/";
-        before = [ "up:k3d" ];
-      };
-      "up:k3d" = {
-        exec = "k3d cluster create workbench --registry-create workbench-registry";
-        status = "k3d cluster get workbench &>/dev/null";
-        before = [ "up:tilt" ];
-      };
-      "down:k3d" = {
-        exec = "k3d cluster delete workbench";
-      };
-      "up:tilt" = {
-        exec = "tilt up";
-        status = "tilt status";
-      };
-    };
-    scripts = lib.mapAttrs' (name: _: {
-      name = "dtr-${name}";
-      value = {
-        exec = "devenv tasks run ${name}";
-      };
-    }) targets;
-    tasks = lib.mapAttrs (name: target: {
-      exec = target.exec;
-    } // lib.optionalAttrs (target ? status) {
-      inherit (target) status;
-    }) targets;
   in {
-    inherit tasks scripts;
 
     # https://devenv.sh/packages/
     packages = with pkgs; [
@@ -45,6 +12,7 @@
       kubectx
       k3d
       kubernetes-helm
+      go-task
     ];
 
     languages.python = {
@@ -59,7 +27,4 @@
       };
     };
 
-    processes.api.exec = "fastapi dev api/app.py";
-
-    # See full reference at https://devenv.sh/reference/options/
   }
