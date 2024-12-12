dab64955bda7c84b6d16c4d2b40a837e8fd77ca9
j4ns8i <jsmalkowski+github@gmail.com>
2024-12-11 21:53:20 -0500

    refactor(api): move /api/v0/{ events => messages } backed by streams

--- api/src/app.py
+++ api/src/app.py
@@ -1,26 +1,31 @@
+from datetime import datetime
 from fastapi import FastAPI, Response
 import fastapi
 from loguru import logger
 import redis.asyncio as _redis
-import xxhash
 
 from . import config
 from .logger import logger
-from .models import NewEventRequest
+from .models import MessageEventData, NewMessageRequest
+from .store import topic_key
 
 app = FastAPI()
 pool = _redis.ConnectionPool(host=config.redis_host, port=config.redis_port, protocol=3)
 redis = _redis.Redis(connection_pool=pool)
 
 
-@app.post("/api/v0/events")
-async def counter(req: NewEventRequest, response: Response):
-    key = xxhash.xxh64_hexdigest(req.topic)
+@app.post("/api/v0/messages")
+async def post_messages(req: NewMessageRequest, response: Response):
+    key = topic_key(req.topic)
     logctx = logger.bind(key=key)
+    data = MessageEventData(
+        topic=req.topic, message=req.message, timestamp=datetime.now()
+    )
     try:
-        await redis.hset(key, mapping={"message": req.message})  # type: ignore
+        logctx.bind(data=data.model_dump()).info("storing new message")
+        await redis.xadd(key, data.model_dump())  # type: ignore
     except Exception as e:
-        logctx.bind(error=str(e)).error("error storing new event")
+        logctx.bind(error=str(e)).error("error storing new message")
         response.status_code = fastapi.status.HTTP_503_SERVICE_UNAVAILABLE
         return {"error": "service unavailable"}
     response.status_code = fastapi.status.HTTP_204_NO_CONTENT
--- api/src/models.py
+++ api/src/models.py
@@ -1,5 +1,24 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Any
+from pydantic import BaseModel, Field, field_serializer
 
-class NewEventRequest(BaseModel):
+
+class EventEnvelope(BaseModel):
+    version: str
+    kind: str
+    data: Any
+
+
+class NewMessageRequest(BaseModel):
     topic: str = Field(min_length=1, max_length=127)
     message: str = Field(min_length=1, max_length=255)
+
+
+class MessageEventData(BaseModel):
+    topic: str = Field(min_length=1, max_length=127)
+    message: str = Field(min_length=1, max_length=255)
+    timestamp: datetime
+
+    @field_serializer("timestamp")
+    def serialize_timestamp(self, timestamp: datetime) -> str:
+        return timestamp.isoformat()
